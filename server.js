5eb82f9725df6e02461a9a6f30577156af7aeef0
Alvaro Silva <alvaro.dasilva@allhenry.com>
2023-03-15 01:09:05 -0300

    updating port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'dist')));
