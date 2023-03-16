fc04593defd6d3703138a3b7bf72679fa47bf744
Alvaro Silva <alvaro.dasilva@allhenry.com>
2023-03-15 21:14:15 -0300

    Fixing typo in webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,7 @@
 // webpack.common.js
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = requier('copy-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.tsx',
